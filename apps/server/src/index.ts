763d5712309625364266853ccebdcdad09b32221
Phillip Nguyen <png9981@gmail.com>
2025-08-24 16:24:16 +0700

    feat: add prisma db

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,14 +1,36 @@
-import { initTRPC } from '@trpc/server';
+import { initTRPC } from "@trpc/server";
+import { z } from "zod";
+import { prisma } from "@repo/db";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import express from "express";
 
-/**
- * Initialization of tRPC backend
- * Should be done only once per backend!
- */
-const t = initTRPC.create();
+const createContext = ({
+  req,
+  res,
+}: trpcExpress.CreateExpressContextOptions) => ({}); // no context
+type Context = Awaited<ReturnType<typeof createContext>>;
 
-/**
- * Export reusable router and procedure helpers
- * that can be used throughout the router
- */
-export const router = t.router;
-export const publicProcedure = t.procedure;
+const t = initTRPC.context<Context>().create();
+const appRouter = t.router({
+  getUser: t.procedure.input(z.string()).query((opts) => {
+    opts.input; // string
+    return { id: opts.input, name: "Bilbo" };
+  }),
+  createUser: t.procedure
+    .input(z.object({ name: z.string().min(5) }))
+    .mutation(async (opts) => {
+      // use your ORM of choice
+      return opts.input;
+    }),
+});
+
+const app = express();
+
+app.use(
+  "/trpc",
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  })
+);
+app.listen(4000);
